d8d0056706068d46dfa4e4d3d2561c3108105fc4
Mael <mael.thomas@data.gouv.fr>
2020-04-18 19:44:22 +0200

    On affiche le contenu markdown

--- Article.js
+++ Article.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import ReactMarkdown from 'react-markdown'
 
 export default ({
 	data: {
 		attributes: { id },
+		body,
 	},
-}) => <div>{id}</div>
+}) => (
+	<div>
+		<ReactMarkdown source={body} />
+	</div>
+)
