ce6304ea65ddebc7f14a22f135ae993e2d36209d
Mael <mael.thomas@data.gouv.fr>
2020-04-28 15:15:35 +0200

    Utilisation d'images imgur moyenne résolution

--- Article.js
+++ Article.js
@@ -7,50 +7,56 @@ export default ({
 		body,
 	},
 }) => (
-	<div
-		css={`
-			max-width: 800px;
-			margin: 0 auto 4rem;
-			h1 {
-				text-align: center;
-			}
-			h2,
-			h3,
-			h4,
-			h5 {
-				margin-top: 2rem;
-			}
-			img {
-				max-width: 80%;
-				margin: 2rem auto;
-				display: block;
-			}
-			img + em {
-				color: #666;
-				text-align: center;
-			}
-			hr {
-				border: 1px solid #eee;
-				width: 70%;
-				margin: 2rem auto;
-			}
-			blockquote {
-				border-left: 6px solid grey;
-				padding-left: 1rem;
-				margin-left: 0;
-			}
-			code {
-				background: #eee;
-				padding: 0.1rem 0.4rem;
-				border-radius: 0.3rem;
-			}
-		`}
-	>
-		<img
-			css="    max-height: 30rem;
-"
-			src={image}
-		></img>
-		<ReactMarkdown source={body} escapeHtml={false} />
+	<div css={() => articleStyle}>
+		<img css="max-height: 30rem;" src={image}></img>
+		<ReactMarkdown
+			renderers={{ image: ImageRenderer }}
+			source={body}
+			escapeHtml={false}
+		/>
 	</div>
 )
+
+const ImageRenderer = ({ src }) => (
+	<img
+		src={src.includes('imgur.com') ? src.replace(/\.(png|jpg)$/, 'h.jpg') : src}
+	/>
+)
+
+const articleStyle = `
+	max-width: 800px;
+	margin: 0 auto 4rem;
+	h1 {
+		text-align: center;
+	}
+	h2,
+	h3,
+	h4,
+	h5 {
+		margin-top: 2rem;
+	}
+	img {
+		max-width: 80%;
+		margin: 2rem auto;
+		display: block;
+	}
+	img + em {
+		color: #666;
+		text-align: center;
+	}
+	hr {
+		border: 1px solid #eee;
+		width: 70%;
+		margin: 2rem auto;
+	}
+	blockquote {
+		border-left: 6px solid grey;
+		padding-left: 1rem;
+		margin-left: 0;
+	}
+	code {
+		background: #eee;
+		padding: 0.1rem 0.4rem;
+		border-radius: 0.3rem;
+	}
+	`
