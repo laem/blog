db24603428ad44fe461174dc096af490574fe108
Mael <mael.thomas@data.gouv.fr>
2021-02-04 16:50:16 +0100

    :bug: Oups, mauvais attribut pour le meta description

--- Meta.js
+++ Meta.js
@@ -8,7 +8,7 @@ export default ({ title, description, image, url, published, updated }) => (
 		<link rel="canonical" href={url} />
 		<meta property="og:title" content={title} />
 		<meta property="og:description" content={description} />
-		<meta property="description" content={description} />
+		<meta name="description" content={description} />
 		<meta property="og:image" content={image} />
 		<meta property="og:url" content={url} />
 		<meta property="og:type" content="article" />
