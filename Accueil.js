25d367ba2c3af1cee105e3849a5219929b0a5c96
Mael <mael.thomas@data.gouv.fr>
2021-12-19 17:57:22 +0100

    1er design pour mes projets

--- Accueil.js
+++ Accueil.js
@@ -3,6 +3,8 @@ import Article from './Article'
 import { imageResizer, accessibleImage } from './Article'
 import { Switch, Route, BrowserRouter as Router, Link } from 'react-router-dom'
 import ScrollToTop from './ScrollToTop'
+import projets from './projets.yaml'
+import styled from 'styled-components'
 
 export const dateCool = (date) =>
 	new Date(date).toLocaleString(undefined, {
@@ -16,6 +18,26 @@ export const pageLayout = `
 		padding: 0.6rem 1rem;
 			max-width: 800px;
 			margin: 0 auto;
+  --shadow-color: 0deg 0% 63%;
+  --shadow-elevation-low:
+    0.3px 0.5px 0.7px hsl(var(--shadow-color) / 0.34),
+    0.4px 0.8px 1px -1.2px hsl(var(--shadow-color) / 0.34),
+    1px 2px 2.5px -2.5px hsl(var(--shadow-color) / 0.34);
+  --shadow-elevation-medium:
+    0.3px 0.5px 0.7px hsl(var(--shadow-color) / 0.36),
+    0.8px 1.6px 2px -0.8px hsl(var(--shadow-color) / 0.36),
+    2.1px 4.1px 5.2px -1.7px hsl(var(--shadow-color) / 0.36),
+    5px 10px 12.6px -2.5px hsl(var(--shadow-color) / 0.36);
+  --shadow-elevation-high:
+    0.3px 0.5px 0.7px hsl(var(--shadow-color) / 0.34),
+    1.5px 2.9px 3.7px -0.4px hsl(var(--shadow-color) / 0.34),
+    2.7px 5.4px 6.8px -0.7px hsl(var(--shadow-color) / 0.34),
+    4.5px 8.9px 11.2px -1.1px hsl(var(--shadow-color) / 0.34),
+    7.1px 14.3px 18px -1.4px hsl(var(--shadow-color) / 0.34),
+    11.2px 22.3px 28.1px -1.8px hsl(var(--shadow-color) / 0.34),
+    17px 33.9px 42.7px -2.1px hsl(var(--shadow-color) / 0.34),
+    25px 50px 62.9px -2.5px hsl(var(--shadow-color) / 0.34);
+
 `
 
 var frontMatterReq = require.context(
@@ -71,7 +93,25 @@ const Header = () => (
 			align-items: center;
 			margin-bottom: 1rem;
 			justify-content: center;
-			> h1 {
+		`}
+	>
+		<img
+			css="width: 3rem; margin: 0 1rem"
+			src="https://avatars1.githubusercontent.com/u/1177762?s=460&v=4"
+		/>
+		<h1>Maël</h1>
+	</header>
+)
+
+const BlogHeader = () => (
+	<div
+		css={`
+			display: flex;
+			align-items: center;
+			margin-bottom: 1rem;
+			justify-content: center;
+			> h1,
+			> h2 {
 				margin: 0.8rem;
 				font-size: 100%;
 				font-weight: normal;
@@ -80,21 +120,114 @@ const Header = () => (
 			> p {
 				margin-top: 0;
 			}
-			img {
+			> img {
 				width: 2.6rem;
+				transform: scale(-1, 1);
 			}
 		`}
-	>
-		<img src="https://avatars1.githubusercontent.com/u/1177762?s=460&v=4" />
-		<h1>
-			Quelques idées sur notre environnement, nos villes et les algorithmes
-		</h1>
-	</header>
+	></div>
+)
+
+const SubHeader = styled.header`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	h2 {
+		font-size: 120%;
+		font-weight: normal;
+		max-width: 20rem;
+		/*
+					border-bottom: 2px solid #fe9d00;
+					padding: 0.2rem 0.4rem;
+					color: #38255c;
+					*/
+	}
+
+	h2 em {
+		background: rgb(78, 47, 90);
+		font-style: normal;
+		color: white;
+		padding: 0rem 0.6rem 0.1rem;
+	}
+	> img {
+		width: 2rem;
+		margin: 0 0.4rem;
+		transform: scale(-1, 1) translateY(0.5rem);
+	}
+`
+
+const Projects = () => (
+	<div>
+		<SubHeader>
+			<img src="https://openmoji.org/data/color/svg/2935.svg" />
+			<h2>
+				<em>Mes projets</em>
+			</h2>
+		</SubHeader>
+		<ul
+			css={`
+				display: flex;
+				flex-wrap: wrap;
+				list-style-type: none;
+				justify-content: center;
+				align-items: center;
+				padding-left: 0;
+				margin: 0 0 2rem;
+				@media (max-width: 800px) {
+					flex-wrap: nowrap;
+					overflow-x: auto;
+					justify-content: normal;
+					height: 11rem;
+					scrollbar-width: none;
+				}
+				li {
+					padding: 0.1rem 0rem;
+					margin: 0.15rem 0.4rem;
+					border-radius: 0.2rem;
+					line-height: 1.6rem;
+					display: flex;
+					flex-direction: column;
+					justify-content: center;
+					align-items: center;
+					box-shadow: var(--shadow-elevation-medium);
+					width: 10rem;
+					height: 8rem;
+					text-align: center;
+				}
+				li img {
+					height: 4rem;
+					max-width: 100%;
+				}
+				li h3 {
+					font-size: 100%;
+					margin: 0.4rem;
+				}
+			`}
+		>
+			{projets.map(({ lien, nom, image, github }) => (
+				<a href={lien}>
+					<li css={``} key={lien}>
+						<img src={image} />
+						<h3>{nom}</h3>
+					</li>
+				</a>
+			))}
+		</ul>
+	</div>
 )
 
 let Liste = ({ articles }) => (
 	<main css={pageLayout}>
 		<Header />
+
+		<Projects />
+		<SubHeader>
+			<img src="https://openmoji.org/data/color/svg/2935.svg" />
+			<h2>
+				<em>Quelques idées</em> sur notre environnement, nos villes et les
+				algorithmes
+			</h2>
+		</SubHeader>
 		<section
 			css={`
 				display: flex;
@@ -140,7 +273,7 @@ let Liste = ({ articles }) => (
 						</header>
 						<Link to={'/' + a.id}>
 							<img
-								css="width: 10rem; box-shadow: rgb(147, 143, 143) 2px 2px 10px 0px;"
+								css="width: 10rem; box-shadow: var(--shadow-elevation-medium)"
 								src={imageResizer('m')(
 									accessibleImage(a.attributes.image).image
 								)}
--- webpack.config.js
+++ webpack.config.js
@@ -44,6 +44,11 @@ module.exports = {
 			},
 
 			{
+				test: /\.ya?ml$/,
+				use: 'yaml-loader',
+				type: 'json',
+			},
+			{
 				test: /\.js$/,
 				exclude: /node_modules/,
 				use: [
