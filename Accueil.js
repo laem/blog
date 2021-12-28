fcfe4564a15dcd1c02082eeb959b0573d88c46b2
Mael <mael.thomas@data.gouv.fr>
2021-12-28 08:18:28 +0100

    Affichage des descriptions de projet

--- Accueil.js
+++ Accueil.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Article from './Article'
 import { imageResizer, accessibleImage } from './Article'
 import { Switch, Route, BrowserRouter as Router, Link } from 'react-router-dom'
@@ -19,7 +19,8 @@ export const pageLayout = `
 			max-width: 800px;
 			margin: 0 auto;
 
-			a {text-decoration: none}
+			a {color: inherit}
+			button {border: none; background: none; cursor: pointer}
   --shadow-color: 0deg 0% 63%;
   --shadow-elevation-low:
     0.3px 0.5px 0.7px hsl(var(--shadow-color) / 0.34),
@@ -167,70 +168,101 @@ const SubHeader = styled.header`
 	}
 `
 
-const Projects = () => (
-	<div>
-		<SubHeader>
-			<img src="https://openmoji.org/data/color/svg/2935.svg" />
-			<h2>
-				<em>Mes projets</em>
-			</h2>
-		</SubHeader>
-		<ul
+const Projects = () => {
+	const [descriptionShown, showDescription] = useState(false)
+
+	const selectedProject = projets.find(({ nom }) => nom === descriptionShown)
+
+	return (
+		<div
 			css={`
-				display: flex;
-				flex-wrap: wrap;
-				list-style-type: none;
-				justify-content: center;
-				align-items: center;
-				padding-left: 0;
 				margin: 0 0 2rem;
-				flex-wrap: nowrap;
-				overflow-x: auto;
-				justify-content: normal;
-				height: 11rem;
-				@media (max-width: 800px) {
-					scrollbar-width: none;
-				}
-				li {
-					padding: 0.1rem 0rem;
-					margin: 0.4rem;
-					border-radius: 0.2rem;
-					line-height: 1.6rem;
-					display: flex;
-					flex-direction: column;
-					justify-content: center;
-					align-items: center;
-					box-shadow: 0.5px 1px 1px hsl(var(--shadow-color) / 0.7);
-					--shadow-color: 0deg 0% 50%;
-					width: 10rem;
-					height: 8rem;
-					text-align: center;
-				}
-				li img {
-					height: 4rem;
-					max-width: 100%;
-					transition: filter 0.3s ease-in;
-				}
-				li img:hover,
-				li img:active {
-				}
-				li h3 {
-					font-size: 100%;
-					margin: 0.4rem;
-				}
 			`}
 		>
-			{projets.map(({ lien, nom, image, github }) => (
-				<a href={lien}>
-					<li css={``} key={lien}>
-						<img src={image} />
-						<h3>{nom}</h3>
-					</li>
-				</a>
-			))}
-		</ul>
-	</div>
-)
+			<SubHeader>
+				<img src="https://openmoji.org/data/color/svg/2935.svg" />
+				<h2>
+					<em>Mes projets</em>
+				</h2>
+			</SubHeader>
+			<ul
+				css={`
+					display: flex;
+					flex-wrap: wrap;
+					list-style-type: none;
+					justify-content: center;
+					align-items: center;
+					padding-left: 0;
+					flex-wrap: nowrap;
+					overflow-x: auto;
+					justify-content: normal;
+					height: 11rem;
+					background: linear-gradient(0% #4d2d5b, 100% #7b3657);
+					@media (max-width: 800px) {
+						scrollbar-width: none;
+					}
+					li {
+						padding: 0.1rem 0rem;
+						margin: 0.4rem;
+						border-radius: 0.2rem;
+						line-height: 1.6rem;
+						display: flex;
+						flex-direction: column;
+						justify-content: center;
+						align-items: center;
+						box-shadow: 0.5px 1px 1px hsl(var(--shadow-color) / 0.7);
+						--shadow-color: 0deg 0% 50%;
+						min-width: 10rem;
+						height: 8rem;
+						text-align: center;
+					}
+					li img {
+						height: 4rem;
+						max-width: 100%;
+						transition: filter 0.3s ease-in;
+					}
+					li img:hover,
+					li img:active {
+					}
+					li h3 {
+						font-size: 100%;
+						margin: 0.4rem;
+					}
+				`}
+			>
+				{projets.map(({ lien, nom, image, github }) => (
+					<button
+						onClick={() =>
+							showDescription(selectedProject?.nom === nom ? false : nom)
+						}
+					>
+						<li css={``} key={lien}>
+							<img src={image} />
+							<h3>{nom}</h3>
+						</li>
+					</button>
+				))}
+			</ul>
+			{descriptionShown && (
+				<div
+					css={`
+						background: #4e2f5a;
+						color: white;
+						padding: 0.6rem 1rem;
+						p {
+							margin: 0 0 0.6rem;
+						}
+					`}
+				>
+					<p>{selectedProject.description}</p>
+					<a href={selectedProject.lien}>
+						{selectedProject.lien.replace('https://', '')}
+					</a>
+				</div>
+			)}
+		</div>
+	)
+}
 
 let Liste = ({ articles }) => (
 	<main css={pageLayout}>
