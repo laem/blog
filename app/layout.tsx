0f48ecceb936f7e2f806fb03934909449d4b2401
Mael <laem@kont.me>
2025-01-19 20:55:23 +0100

    Jaquette meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ export const metadata = {
 	title: 'Le blog de Maël THOMAS-QUILLÉVÉRÉ',
 	description: `Bienvenue sur mon blog. J'y présente mes projets personnels et professionnels, ainsi que quelques articles et essais sur l'écologie, l'urbanisme (nos villes, notre territoire, le vélo et la voiture), la politique, la sociologie, la pédagogie sur les algorithmes.`,
 	metadataBase: new URL('https://kont.me'),
+	openGraph: {
+		images: ['/jaquette.png'],
+	},
 }
 
 export default function RootLayout({
